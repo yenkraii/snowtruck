4284fa5f14dc970a0691ccf603043093fea70ee8
Qi Yuan Wong <s10222150@connect.np.edu.sg>
2023-07-03 14:25:10 +0800

    plox

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,11 @@ st.set_page_config(page_title="SnowTruck", page_icon=":truck:")
 
 #st.image(snowtruck_logo)
 title_container = st.beta_container()
-fp = open("images/Snowtruck_Icon.jpeg","rb")
+fp = open("images/Snowtruck_ProductBox.jpeg","rb")
 snowtruck_logo = PIL.Image.open(fp)
 
-col1, col2 = st.columns([1, 20])
-with title_container:
-    with col1:
-        st.image(snowtruck_logo, width=64)
-    with col2:
-        st.markdown('<h1 style="color: blue;">SnowTruck</h1>',
-                    unsafe_allow_html=True)
-#st.title("SnowTruck:minibus:")
+st.image(snowtruck_logo,width=64)
+st.title("SnowTruck:minibus:")
 
 # connects to the snowflake account 
 # if need to use the data there
