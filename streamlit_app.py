ecb3047917b8e498f4d4823b3152e8c8b5366155
kiara <102582449+yenkraii@users.noreply.github.com>
2023-07-03 12:31:58 +0800

    Update streamlit_app.py
    added in the tabs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,29 @@
 import streamlit as st
+import pandas as pd
+import snowflake.connector
 
-st.title("hello world!")
+st.set_page_config(page_title="SnowTruck", page_icon=":snowflake:")
+
+st.title("SnowTruck")
+
+# connects to the snowflake account 
+# if need to use the data there
+conn = snowflake.connector.connect(**st.secrets["snowflake"])
+
+# tabs
+tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1","tab2","tab3","tab4","tab5"])
+
+with tab1:
+  st.write()
+
+with tab2:
+  st.write()
+
+with tab3:
+  st.write()
+
+with tab4:
+  st.write()
+
+with tab5:
+  st.write()
