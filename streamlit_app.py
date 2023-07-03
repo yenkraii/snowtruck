312957b4562e82b268f8807931ef71ee7a4f5042
Qi Yuan Wong <s10222150@connect.np.edu.sg>
2023-07-03 14:18:42 +0800

    title yay

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,21 @@ import pandas as pd
 import PIL.Image
 import snowflake.connector
 
+st.set_page_config(page_title="SnowTruck", page_icon=":truck:")
+
+#st.image(snowtruck_logo)
+title_container = st.beta_container()
 fp = open("images/Snowtruck_Icon.jpeg","rb")
 snowtruck_logo = PIL.Image.open(fp)
 
-st.set_page_config(page_title="SnowTruck", page_icon=":truck:")
-
-st.title("SnowTruck:minibus:", st.image(snowtruck_logo) )
+col1, col2 = st.beta_columns([1, 20])
+with title_container:
+    with col1:
+        st.image(snowtruck_logo, width=64)
+    with col2:
+        st.markdown('<h1 style="color: blue;">SnowTruck</h1>',
+                    unsafe_allow_html=True)
+#st.title("SnowTruck:minibus:")
 
 # connects to the snowflake account 
 # if need to use the data there
