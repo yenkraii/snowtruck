fc5fd5c96bb06ed74ce463b2ddc2c3d3b44e5c4d
AletheaChan <116297612+AletheaChan@users.noreply.github.com>
2023-07-12 01:09:35 +0800

    Some dropdowns

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,63 @@ conn = snowflake.connector.connect(**st.secrets["snowflake"])
 tab1,tab2,tab3,tab4,tab5 = st.tabs(["tab1","tab2","tab3","tab4","tab5"])
 
 with tab1:
-  st.write()
+  # Define the app title and favicon
+  st.write('How much can you make from the TastyBytes locations?)
+  st.markdown("This tab allows you to make predictions on the price of a listing based on the neighbourhood and room type. The model used is a Random Forest Regressor trained on the Airbnb Singapore listings dataset.")
+  st.write('Choose a Truck Brand Name, City, Truck Location and Time Frame to get the predicted monetary sales.')
+
+
+  # Load the cleaned and transformed dataset
+  df = pd.read_csv('df_alethea.csv')
+  quantity = df[['TOTAL_QUANTITY']]
+  
+  dow_mapping={'Monday':0,'Tuesday':1,'Wednesday':2,'Thursday':3,'Friday':4,'Saturday':5,'Sunday':6}
+  dow_reverse_mapping = {v: k for k, v in dow_mapping.items()}
+  dow_labels = [dow_reverse_mapping[i] for i in sorted(dow_reverse_mapping.keys())]
+
+  bn_mapping = { "Cheeky Greek": 0,
+                "Guac n' Roll": 1,
+                "Smoky BBQ": 2,
+                "Peking Truck": 3,
+                "Tasty Tibs": 4,
+                "Better Off Bread": 5,
+                "The Mega Melt": 6,
+                "Le Coin des Crêpes": 7,
+                "The Mac Shack": 8,
+                "Nani's Kitchen": 9,
+                "Plant Palace": 10,
+                "Kitakata Ramen Bar": 11,
+                "Amped Up Franks": 12,
+                "Freezing Point": 13,
+                "Revenge of the Curds": 14 }
+
+  ct_mapping = {'San Mateo': 0, 'Seattle': 1, 'New York City': 2, 'Boston': 3, 'Denver':4}
+
+  def get_dayOfWeek():
+    dayOfWeek = st.selectbox('Select a day of week', dow_labels)
+    return dayOfWeek
+        
+  def get_truckBrandName(TRUCK_BRAND_NAME):
+    TRUCK_BRAND_NAME = df[df['DAY_OF_WEEK'] == dow_mapping[DAY_OF_WEEK]]['TRUCK_BRAND_NAME'].unique()
+    TRUCK_BRAND_NAME = st.selectbox('Select the truck's brand name', bn_mapping)
+    return TRUCK_BRAND_NAME
+        
+  def get_truckCity(CITY):
+  CITY = df[df['TRUCK_BRAND_NAME'] == bn_mapping[TRUCK_BRAND_NAME]]['CITY'].unique()
+  CITY = st.selectbox('Select a city', ct_mapping)
+  return CITY
+
+  # Define the user input fields
+  dow_input = get_dayOfWeek()
+  bn_input = get_truckBrandName(dow_input)
+  ct_input = get_truckCity(bn_mapping)
+
+  # Map user inputs to integer encoding
+  dow_int = dow_mapping[dow_input]
+  bn_input = bn_mapping[bn_input]
+  ct_input = ct_mapping[ct_input]
+
+
 
 with tab2:
 
