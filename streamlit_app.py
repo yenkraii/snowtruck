e626a69cda44413675f4a3f4aaab26adc7470356
okxinle <99386726+okxinle@users.noreply.github.com>
2023-07-13 10:03:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,8 @@ with tab2:
     st.markdown("This tab allows you to make predictions on the price of a listing based on the neighbourhood and room type. The model used is a Random Forest Regressor trained on the Airbnb Singapore listings dataset.")
     st.write('Choose a neighborhood group, neighborhood, and room type to get the predicted average price.')
     
-    with open('xgb_xinle.pkl', 'rb') as file:
-        xgb = pickle.load(file)
+    with open('rd_xinle.pkl', 'rb') as file:
+        rf = pickle.load(file)
 
     # Load the cleaned and transformed dataset
     df = pd.read_csv('df_xinle.csv')
@@ -149,17 +149,17 @@ with tab2:
         
         # Make the prediction   
         input_data = [[dow_int,mt_int,ic_int,isc_int]]
-        input_df = pd.DataFrame(input_data, columns=['DAY_OF_WEEK', 'MENU_ITEM_NAME', 'MENU_TYPE','ITEM_CATEGORY','ITEM_SUBCATEGORY', 'UNIT_PRICE'])
+        input_df = pd.DataFrame(input_data, columns=['DAY_OF_WEEK', 'MENU_ITEM_NAME', 'MENU_TYPE','ITEM_CATEGORY','ITEM_SUBCATEGORY'])
         prediction = rf.predict(input_df)   
         # convert output data and columns, including price, to a dataframe avoiding TypeError: type numpy.ndarray doesn't define round method
         output_data = [DAY_OF_WEEK, MENU_ITEM_NAME, MENU_TYPE,ITEM_CATEGORY,ITEM_SUBCATEGORY, UNIT_PRICE, prediction[0]]
 
     
-        output_df = pd.DataFrame([output_data], columns=['DAY_OF_WEEK', 'MENU_ITEM_NAME', 'MENU_TYPE','ITEM_CATEGORY','ITEM_SUBCATEGORY', 'UNIT_PRICE', 'predicted_quantity'])
+        output_df = pd.DataFrame([output_data], columns=['DAY_OF_WEEK', 'MENU_ITEM_NAME', 'MENU_TYPE','ITEM_CATEGORY','ITEM_SUBCATEGORY', 'predicted_quantity'])
 
         # Make the prediction   
         # show prediction on price in dollars and cents using the price column 
-        input_data = [[dow_int, MENU_ITEM_NAME, mt_int,ic_int,isc_int, UNIT_PRICE]]
+        input_data = [[dow_int, MENU_ITEM_NAME, mt_int,ic_int,isc_int]]
 
         predicted_price = xgb.predict(input_df)[0]
         st.write('The predicted average price is ${:.2f}.'.format(predicted_quantity))
