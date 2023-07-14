8c9a1ecc499c38c3e2543363fa275fe3d00598be
okxinle <99386726+okxinle@users.noreply.github.com>
2023-07-14 17:43:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -433,8 +433,8 @@ with tab2:
 
   # Define the app title and favicon
     st.title('Profit Prediction for Menu Items') 
-    st.markdown("This tab allows you to make predictions on the profit of menu items based on different variables.
-    The model used is an XGBoost Regressor trained on the TastyBytes dataset.")
+    st.markdown("This tab allows you to make predictions on the profit of menu items based on different variables. \
+                 The model used is an XGBoost Regressor trained on the TastyBytes dataset.")
     
     with open('xgb_xinle.pkl', 'rb') as file:
         xgb_xinle = pickle.load(file)
