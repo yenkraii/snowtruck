ddbc299431f68991e9c5588e4bc925d1734902bc
kiara <102582449+yenkraii@users.noreply.github.com>
2023-07-28 04:37:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -623,9 +623,11 @@ with tab3:
   
   if st.button("Suggest!", key= 999):
     # suggest based on market basket analysis
+    st.write(arm)
     first2 = str(menu_dict[first_item]) + "," + str(menu_dict[second_item])
     st.write(first2)
     filtered_df = arm[arm["first2"] == first2].sort_values(by="confidence", ascending = False)
+    st.write(filtered_df)
     # top suggestion 
     top_c = filtered_df.iloc[:1,:]
     st.write(top_c)
