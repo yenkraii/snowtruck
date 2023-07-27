42955161a771f486ccac8bdecc817346764be061
kiara <102582449+yenkraii@users.noreply.github.com>
2023-07-28 06:03:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -632,9 +632,9 @@ with tab3:
     top_c = filtered_df.head(1)
     st.write(top_c)
     
-    consequent = top_c.loc[0,"consequents"]
+    consequent = top_c.loc[:,"consequents"]
     st.write("Based on market basket analysis, {0} is recommended.".format(consequent))
-    confidence = top_c["confidence"]
+    confidence = top_c.loc[:,"confidence"]
     consequent = menu_dict[consequent]
     
   
