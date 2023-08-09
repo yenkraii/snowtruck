de583cf6f6ae4304212f0de6e12aece7918c32a1
kiara <102582449+yenkraii@users.noreply.github.com>
2023-08-09 21:54:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -789,10 +789,14 @@ with tab3:
       st.session_state.conf = 0
       
     data_input = [[st.session_state.cons, st.session_state.conf, menu_dict[first_item], menu_dict[second_item], c_gender, c_marital, c_child, c_age, c_member]]
-    input_df = pd.DataFrame(data = data_input, columns = ["consequents", "confidence", 0, 1, "GENDER", "MARITAL_STATUS", "CHILDREN_COUNT", "AGE", "MEMBERSHIP"])
-    st.write(input_df)
-    uplift_score = slearner.predict(input_df)
-    st.write(uplift_score)
+    input_df = pd.DataFrame(data = data_input, columns = ["consequents", "confidence", 0, 1, "GENDER", "MARITAL_STATUS", "CHILDREN_COUNT", "AGE", "MEMBERSHIP"]))
+    uplift_score = slearner.predict_proba(input_df)
+    if uplift_score < -0.75:
+      st.write("Best not to disturb! Better not push them.")
+    else if uplift_score > 0.25:
+      st.write("They won't be affected by the promotions!")
+    else:
+      st.write("They are persuadables! You can persuade them.")
 
 with tab4:
   import streamlit as st
